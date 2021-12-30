22ee4563e68b1a83c263e69f7e6f84f87b6ccb86
Abdullah Awan <mlkabawan336@gmail.com>
2021-12-31 00:29:48 +0500

    Space Shooter

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,7 @@ using namespace std;
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 #define SPACE 32
+#define ESC 27
 
 void shootAlien(char aliens[][150], int x, int y, int& x1, int& y1, int& x2, int& y2, int& x3, int& y3, int& x4, int& y4, int& x5, int& y5, int key, int& score);
 
@@ -26,7 +27,7 @@ int getInput(int key)
     }
 
     return false;
-};
+}
 
 void setCursorPointer(int x = 0, int y = 0)
 {
@@ -70,8 +71,8 @@ void drawGameFrame(int a_x, int a_y, int b_x, int b_y)
 
 void gameOverDefeatMessage()
 { // When you lose the game you see this in screen
-    int a_x = 77;
-    int a_y = 25;
+    int a_x = 94;
+    int a_y = 20;
     int b_x = a_x + 23;
     int b_y = a_y + 4;
     setCursorPointer(a_x + 1, a_y + 2); printf("      DEFEAT!!!");
@@ -101,12 +102,14 @@ void drawGameLimits()
     // Draws the game limits, and information that doesn't have to be printed repeatedly
     drawGameFrame(1, 1, 150, 49); // The default size of the Windows terminal is 25 rows x 80 columns
     drawScoreFrame(153, 1, 209, 49); // The default size of the Windows terminal is 25 rows x 80 columns
-    setCursorPointer(160, 2);
-    printf("HP: ");
-    setCursorPointer(160, 4);
-    printf("Score: ");
     setCursorPointer(160, 10);
-    printf("Top 5 Score: ");
+    cout << "LIVES: ";
+    setCursorPointer(160, 15);
+    cout << "SCORE: ";
+    setCursorPointer(160, 5);
+    cout << "LEVEL: ";
+    setCursorPointer(160, 25);
+    cout << "TOP 5 SCORE: ";
 }
 
 void welcomeMessage()
@@ -151,30 +154,24 @@ void buildSpaceShip(int& x1, int& y1, int& x2, int& y2, int& x3, int& y3, int& x
     }
 
     setCursorPointer(x1, y1);
-    printf("%c", 17);                     //  printf("%c", 17);
+    printf("%c", 17);
     setCursorPointer(x2, y2);
-    printf("%c", 223);                 //  printf("     %c", 16);
+    printf("%c", 223);
     setCursorPointer(x3, y3);
     printf("%c", 16);
     setCursorPointer(x4, y4);
-    printf("%c", 4);                 //  printf("  %c   ", 4);
+    printf("%c", 4);
     setCursorPointer(x5, y5);
-    printf("%c", 30);               //   printf("   %c  ", 30);
+    printf("%c", 30);
 }
 
 void createAliens(char aliens[][150]) {
-    int x = 0;
-    int y = 2;
+    int x = rand() % 148;
+    x = x + 2;
 
-    for (int i = 0; i < 1; i++) {
-        x = rand() % 148;
-        x = x + 2;
-
-        setCursorPointer(x, y);
-        aliens[y][x] = 'X';
-        Sleep(100);
-        cout << aliens[y][x];
-    }
+    setCursorPointer(x, 2);
+    aliens[2][x] = 'X';
+    cout << aliens[2][x];
 }
 
 void performOperation(char aliens[][150], int& x1, int& y1, int& x2, int& y2, int& x3, int& y3, int& x4, int& y4, int& x5, int& y5, int key, bool isShoot, int& score)
@@ -218,57 +215,41 @@ void performOperation(char aliens[][150], int& x1, int& y1, int& x2, int& y2, in
     }
 }
 
-int moveAliens(char aliens[][150], int& x1, int& y1, int& x2, int& y2, int& x3, int& y3, int& x4, int& y4, int& x5, int& y5, int key, int& score)
+void moveAliens(char aliens[][150], int& x1, int& y1, int& x2, int& y2, int& x3, int& y3, int& x4, int& y4, int& x5, int& y5, int key, int& score, bool& isKilled, int& speed)
 {
-    bool isKilled = false;
     char tempAliens[47][150] = { 0 };
 
-    while (!isKilled) {
-        for (int i = 0; i < 47; i++) {
-            for (int j = 0; j < 150; j++) {
-                tempAliens[i][j] = aliens[i][j];
-            }
+    for (int i = 0; i < 47; i++) {
+        for (int j = 0; j < 150; j++) {
+            tempAliens[i][j] = aliens[i][j];
         }
+    }
 
-        for (int i = 2; i < 47; i++) {
-            for (int j = 2; j < 150; j++) {
-                if (tempAliens[i][j] == 'X') {
-                    setCursorPointer(j, i);
-                    aliens[i][j] = ' ';
-                    cout << aliens[i][j];
+    for (int i = 2; i < 47; i++) {
+        for (int j = 2; j < 150; j++) {
+            if (tempAliens[i][j] == 'X') {
+                setCursorPointer(j, i);
+                aliens[i][j] = ' ';
+                cout << aliens[i][j];
 
-                    setCursorPointer(j, i + 1);
-                    aliens[i + 1][j] = 'X';
-                    cout << aliens[i + 1][j];
+                setCursorPointer(j, i + 1);
+                aliens[i + 1][j] = 'X';
+                cout << aliens[i + 1][j];
 
-                    if (aliens[46][j] == 'X')
-                    {
-                        isKilled = true;
-                    }
+                if (aliens[46][j] == 'X')
+                {
+                    isKilled = true;
                 }
             }
         }
-        if (isKilled != true) {
-
-            performOperation(aliens, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, key, false, score);
-
-            createAliens(aliens);
-
-            performOperation(aliens, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, key, false, score);
-        }
-        else {
-            system("cls");
-            gameOverDefeatMessage();
-            _getch();
-            return 0;
-        }
     }
+
+    createAliens(aliens);
 }
 
 void shootAlien(char aliens[][150], int x, int y, int& x1, int& y1, int& x2, int& y2, int& x3, int& y3, int& x4, int& y4, int& x5, int& y5, int key, int& score) {
     while (aliens[y][x] != 'X') {
         performOperation(aliens, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, key, true, score);
-
         if (y == 2) {
             break;
         }
@@ -288,7 +269,7 @@ void shootAlien(char aliens[][150], int x, int y, int& x1, int& y1, int& x2, int
         aliens[y][x] = ' ';
         score++;
 
-        setCursorPointer(168, 4);
+        setCursorPointer(170, 15);
         cout << score;
     }
 }
@@ -303,20 +284,65 @@ int main()
 
     welcomeMessage();
     _getch();
-    system("cls");
-    drawGameLimits();
 
-    int key = 0, score = 0;
-    int x1 = 73, y1 = 48, x2 = 75, y2 = 48, x3 = 77, y3 = 48, x4 = 75, y4 = 47, x5 = 75, y5 = 46;
-    char aliens[47][150] = { 0 };
+    int speed = 15000;
+    int level = 1;
+    int lives = 3;
+    int interval = 0;
+    int Exit = 0;
 
-    buildSpaceShip(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, 0);
+    while (lives > 0 && level <= 5) {
+        system("cls");
+        drawGameLimits();
 
-    while (1)
-    {
-        performOperation(aliens, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, key, false, score);
+        int key = 0, score = 0;
+        int x1 = 73, y1 = 48, x2 = 75, y2 = 48, x3 = 77, y3 = 48, x4 = 75, y4 = 47, x5 = 75, y5 = 46;
+        char aliens[47][150] = { 0 };
+        bool isKilled = false;
+
+        buildSpaceShip(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, 0);
         createAliens(aliens);
-        moveAliens(aliens, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, key, score);
+
+        setCursorPointer(170, 10);
+        cout << lives;
+
+        setCursorPointer(170, 5);
+        cout << level;
+
+        while (!isKilled && score < 2)
+        {
+            performOperation(aliens, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, key, false, score);
+
+            if (interval > speed) {
+                moveAliens(aliens, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, key, score, isKilled, speed);
+                interval = 0;
+            }
+
+            interval++;
+
+        }
+
+        if (isKilled) {
+            system("cls");
+            gameOverDefeatMessage();
+            _getch();
+            lives--;
+        }
+        else {
+
+            system("cls");
+            setCursorPointer(98, 22);
+            if (level != 5) {
+                cout << "LEVEL " << level << " COMPLETE!!!";
+            }
+            _getch();
+            speed = speed - 2000;
+            level++;
+        }
     }
-    return 0;
+    if (lives == 5) {
+        setCursorPointer(98, 22);
+        cout << "VICTORY";
+        _getch();
+    }
 }
